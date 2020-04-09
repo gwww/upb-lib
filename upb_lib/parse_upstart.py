d28f1c4046aa7cbff0b7ecd5a974b72f0fad7bd7
Glenn Waters <gwwaters@gmail.com>
2020-04-09 14:24:22 -0400

    Refactor to cleanup code.

--- upb_lib/parse_upstart.py
+++ upb_lib/parse_upstart.py
@@ -23,62 +23,70 @@ def process_upstart_file(pim, filename):
 def _process_file(pim, file):
     for line in file:
         fields = line.strip().split(",")
-
-        # File overview record
         if fields[0] == "0":
+            # File overview record
             network_id = int(fields[4])
-
-        # Link definition record
         elif fields[0] == "2":
-            link_id = int(fields[1])
-            link = Link(LinkAddr(network_id, link_id), pim)
-            link.name = fields[2]
-            pim.links.add_element(link)
-
-        # Light record
+            _link_definition_record(pim, network_id, fields)
         elif fields[0] == "3":
-            # network_id used in future reads, until it changes
-            upb_id = int(fields[1])
-            network_id = int(fields[2])
-            number_of_channels = int(fields[8])
-            multi_channel = number_of_channels > 1
-            for channel in range(0, number_of_channels):
-                light = Light(UPBAddr(network_id, upb_id, channel, multi_channel), pim)
-                if multi_channel:
-                    light.name = f"{fields[11]} {fields[12]} {channel}"
-                else:
-                    light.name = f"{fields[11]} {fields[12]}"
-                light.version = f"{fields[5]}.{fields[6]}"
-
-                product = f"{fields[3]}/{fields[4]}"
-                if product in PRODUCTS:
-                    light.product = PRODUCTS[product][0]
-                    light.kind = PRODUCTS[product][1]
-                else:
-                    light.product = product
-                    light.kind = fields[7]
-
-                pim.lights.add_element(light)
-
-        # Channel info record, only care about dimmable flag
+            _device_definition_record(pim, network_id, fields)
         elif fields[0] == "8":
-            light_id = UPBAddr(network_id, fields[2], fields[1]).index
-            light = pim.lights.elements[light_id]
-            light.dimmable = True if fields[3] == "1" else False
-
-        # Light link definition
+            _channel_definition_record(pim, network_id, fields)
         elif fields[0] == "4":
-            link_id = int(fields[4])
-            if link_id == 255:
-                continue
-
-            link_idx = LinkAddr(network_id, link_id).index
-            light_idx = UPBAddr(network_id, fields[3], fields[1]).index
-            dim_level = int(fields[5])
-            pim.links[link_idx].add_light(LightLink(light_idx, dim_level))
-
-        # Rename definition - not part of UPE file spec
+            _link_device_definition_record(pim, network_id, fields)
         elif fields[0] == "99":
-            light = pim.lights.elements.get(fields[1])
-            if light:
-                light.name = fields[2]
+            _rename_device_record(pim, fields)
+
+
+def _link_definition_record(pim, network_id, fields):
+    link_id = int(fields[1])
+    link = Link(LinkAddr(network_id, link_id), pim)
+    link.name = fields[2]
+    pim.links.add_element(link)
+
+
+def _device_definition_record(pim, network_id, fields):
+    upb_id = int(fields[1])
+    number_of_channels = int(fields[8])
+    multi_channel = number_of_channels > 1
+    for channel in range(0, number_of_channels):
+        light = Light(UPBAddr(network_id, upb_id, channel, multi_channel), pim)
+        if multi_channel:
+            light.name = f"{fields[11]} {fields[12]} {channel}"
+        else:
+            light.name = f"{fields[11]} {fields[12]}"
+        light.version = f"{fields[5]}.{fields[6]}"
+
+        product = f"{fields[3]}/{fields[4]}"
+        if product in PRODUCTS:
+            light.product = PRODUCTS[product][0]
+            light.kind = PRODUCTS[product][1]
+        else:
+            light.product = product
+            light.kind = fields[7]
+
+        pim.lights.add_element(light)
+
+
+def _channel_definition_record(pim, network_id, fields):
+    light_id = UPBAddr(network_id, fields[2], fields[1]).index
+    light = pim.lights.elements.get(light_id)
+    if light:
+        light.dimmable = fields[3] == "1"
+
+
+def _link_device_definition_record(pim, network_id, fields):
+    link_id = int(fields[4])
+    if link_id == 255:
+        return
+
+    link_idx = LinkAddr(network_id, link_id).index
+    light_idx = UPBAddr(network_id, fields[3], fields[1]).index
+    dim_level = int(fields[5])
+    pim.links[link_idx].add_light(LightLink(light_idx, dim_level))
+
+
+def _rename_device_record(pim, fields):
+    light = pim.lights.elements.get(fields[1])
+    if light:
+        light.name = fields[2]
