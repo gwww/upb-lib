9693e93e3556daa62528ebfb9011bc22389d30e6
Glenn Waters <gwwaters@gmail.com>
2021-01-01 21:29:18 -0500

    Fix tests.

--- test/test_proto.py
+++ test/test_proto.py
@@ -5,14 +5,14 @@ from upb_lib.proto import Connection
 
 def test_receive_single_packet_and_pass_to_handler():
     mock_gotdata = Mock()
-    connection = Connection(None, None, None, mock_gotdata, None)
+    connection = Connection(None, 0, None, None, mock_gotdata, 0)
     connection.data_received("PU0800C2FF068600AB\r".encode())
     mock_gotdata.assert_called_once_with(bytearray(b"\x08\x00\xc2\xff\x06\x86\x00"))
 
 
 def test_receive_multiple_packets():
     mock_gotdata = Mock()
-    connection = Connection(None, None, None, mock_gotdata, None)
+    connection = Connection(None, 0, None, None, mock_gotdata, 0)
     connection.data_received("PU0800C2FF068600AB\rPU0800C2FF068600AB\r".encode())
     mock_gotdata.assert_has_calls(
         [
@@ -24,7 +24,7 @@ def test_receive_multiple_packets():
 
 def test_received_full_packet_and_partial_packet():
     mock_gotdata = Mock()
-    connection = Connection(None, None, None, mock_gotdata, None)
+    connection = Connection(None, 0, None, None, mock_gotdata, 0)
     connection.data_received("PU8800C2FF068600AB\rPU0".encode())
     mock_gotdata.assert_called_once_with(bytearray(b"\x88\x00\xc2\xff\x06\x86\x00"))
 
